948a1ced5f7ec26fe810a49b423ce44d66bf578b
Pskmax <69136130+Pskmax@users.noreply.github.com>
2021-09-08 13:00:37 +0700

    Delete Python directory

--- Python/Other/test01.py
+++
@@ -1,6 +0,0 @@
-x = 555 % 277
-print(x)
-y = 555/277
-print(y)
-z = str(int(y)) + str(x)
-print(z)
--- Python/week1/election.py
+++
@@ -1,9 +0,0 @@
-n,p,vote = int(input('*** Election ***'+'\nEnter a number of voter(s) : ')),input().split(' '),[]
-for i in p:
-    if(int(i) > 0 and int(i) <=20) :
-        vote.append(int(i))
-
-if len(vote)<1 :
-     print('*** No Candidate Wins ***')
-elif len(vote)>0:
-    print((max(vote,key=vote.count)))
