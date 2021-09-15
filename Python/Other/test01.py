d017e832348df322d351e6188e004a470c92fb4b
Pskmax <69136130+Pskmax@users.noreply.github.com>
2021-09-15 16:49:31 +0700

    Delete Python/Other directory

--- Python/Other/test01.py
+++
@@ -1,8 +0,0 @@
-def reverseSort(lst):
-    if not lst: 
-        return lst
-    return lst[-1:] + reverseSort(lst[:-1])
-
-lst =  list(map(int,input('Enter Input : ').split(',')))
-lst.sort()
-print('List after Sorted :',reverseSort(lst))
